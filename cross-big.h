9084e3887e4f60d1765d96ca0b59115568972205
Andrej Shadura <andrew@shadura.me>
2015-03-23 19:11:26 +0100

    Strip whitespace from crosses

--- cross-big.h
+++ cross-big.h
@@ -1,4 +1,4 @@
 #define cross_big_width 120
-#define cross_big_height 144
-extern const char cross_big_lines[7][cross_big_width / 8];
+#define cross_big_height 96
+extern const char cross_big_lines[5][cross_big_width / 8];
 extern const unsigned char cross_big_bits[cross_big_height];
--- cross-small.h
+++ cross-small.h
@@ -1,4 +1,4 @@
 #define cross_small_width 120
-#define cross_small_height 144
+#define cross_small_height 96
 extern const char cross_small_lines[5][cross_small_width / 8];
 extern const unsigned char cross_small_bits[cross_small_height];
